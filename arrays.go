cec594f95604d405ea86efa462eb2b8b0362d889
williamneo <williamneokh@gmail.com>
2022-04-01 21:00:45 +0800

    Completed

--- arrays.go
+++ arrays.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert Code here
+	var OS [6]string
+
+	OS[0] = "Windows XP"
+	OS[1] = "Linux 1.0"
+	OS[2] = "Raspbian Teensy"
+	OS[3] = "MacOS"
+	OS[4] = "IOS"
+	OS[5] = "Google Android"
+
+	for i, length := range OS {
+		fmt.Printf("%v %v has length of %v\n", i+1, length, len(OS[i]))
+	}
 }
